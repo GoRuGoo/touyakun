99feb63825db30ad658a436b01e6d40b315dfcf9
GoRuGoo <dachanyr8989@gmail.com>
2024-04-26 23:37:34 +0900

    feat: routerの基本形実装完了

---
+++ api/router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	r.Use(cors.New(config))
+
+	return r
+}
