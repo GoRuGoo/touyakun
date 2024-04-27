d78d25627e188106fde17e3642c2a06233f8783b
rtasan <rtasansax@gmail.com>
2024-04-27 22:55:13 +0900

    feat:薬の情報のスキーマを定義

---
+++ ai/app/schemas/medications.py
@@ -0,0 +1,15 @@
+from typing import List
+from pydantic import BaseModel, Field
+
+
+class Medication(BaseModel):
+    name:str
+    morning:bool
+    afternoon:bool
+    evening:bool
+    dosage:int
+    duration_days:int
+
+
+class Medications(BaseModel):
+    medications:List[Medication]
