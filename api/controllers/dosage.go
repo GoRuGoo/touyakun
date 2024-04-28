064c25ab6a080f1be4de38b229158e2a47536694
GoRuGoo <dachanyr8989@gmail.com>
2024-04-28 12:25:21 +0900

    fix: テスト時の間違ったものが入ってしまったため修正

--- api/controllers/dosage.go
+++ api/controllers/dosage.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"errors"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"touyakun/models"
@@ -28,8 +27,7 @@ func (dc *DosageController) GetMedications(c *gin.Context) {
 		c.Error(errors.New("auth_key is not a string")).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusUnauthorized, "auth_key is not a string", "auth_key is not a string"})
 		return
 	}
-	fmt.Println(varidatedAuthKey)
-	medications, err := dc.dosageModel.GetMedications("hoge")
+	medications, err := dc.dosageModel.GetMedications(varidatedAuthKey)
 	if err != nil {
 		c.Error(err).SetType(gin.ErrorTypePublic).SetMeta(APIError{http.StatusNotFound, "failed to get medications", "failed to get medications"})
 		return
