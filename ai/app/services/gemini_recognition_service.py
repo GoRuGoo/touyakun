b5b0d4b748258f810a06d39ffbe637970676a493
rtasan <rtasansax@gmail.com>
2024-04-28 21:00:23 +0900

    feat:認識に失敗したときの例外処理を実装

--- ai/app/services/gemini_recognition_service.py
+++ ai/app/services/gemini_recognition_service.py
@@ -2,6 +2,7 @@ from vertexai.preview.generative_models import GenerativeModel, Image
 from app.schemas.medications import Medications, Medication
 import json
 from typing import AsyncIterable, Union
+from fastapi import HTTPException
 
 
 class GeminiRecognitionService:
@@ -26,8 +27,8 @@ class GeminiRecognitionService:
         # VertexAIのAPIを呼ぶ
         response = await self.model.generate_content_async(
             [
-                """この画像から、薬の名前、朝昼夜のうちいつ何錠の薬を何日間にわたって飲めばよいかを認識し、json形式で返してください。
-        以下は返却するjson形式の例です。
+                r"""この画像から、薬の名前、朝昼夜のうちいつ何錠の薬を何日間にわたって飲めばよいかを認識し、json形式で返してください。認識に失敗したときは、認識に失敗しました。というエラーメッセージを返してください。
+        以下は認識に成功したときに返却するjson形式の例です。
         {
             "medications": [
                 {
@@ -47,13 +48,28 @@ class GeminiRecognitionService:
                     "duration_days": 20
                 }
             ]
-        }""",
+        }
+        """,
                 img,
             ]
         )
         # ここでresponseをパースしてMedicationsオブジェクトに変換して返す
-        res = response.text.replace(" ```json\n", "").replace("\n```", "")
-        json_dict = json.loads(res)
+        # エラー処理
+        if "認識に失敗しました。" in response.text:
+            raise HTTPException(
+                status_code=500,
+                detail="Error: Failed to recognize medications from image. "
+                "(Data returned from VertexAI API was empty. Could be due to invalid image or other reasons.)",
+            )
+        try:
+            res = response.text.replace(" ```json\n", "").replace("\n```", "")
+            json_dict = json.loads(res)
+        except Exception as e:
+            raise HTTPException(
+                status_code=500,
+                detail="Error: Failed to parse response from VertexAI. "
+                "(Data returned from VertexAI API was invalid.)",
+            ) from e
         return json_dict
 
 
