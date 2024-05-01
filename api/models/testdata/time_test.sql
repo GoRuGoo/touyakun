28c28bf04ceb9843058fc11376782689daabd147
GoRuGoo <dachanyr8989@gmail.com>
2024-05-01 23:01:30 +0900

    feat: gettimelist関数を実装

--- api/models/testdata/time_test.sql
+++ api/models/testdata/time_test.sql
@@ -1,5 +1,6 @@
 DELETE
 FROM public.users
 WHERE id = 1;
-INSERT INTO public.users (id, line_user_id, created_at, updated_at)
-VALUES (1, 'test_id', '2024-04-27 17:10:04.675314', '2024-04-27 17:10:04.675314');
+
+INSERT INTO public.users (id, line_user_id, morning_medication_time, afternoon_medication_time, evening_medication_time)
+VALUES (1, 'test_id', '08:00:00', '12:00:00', '18:00:00');
