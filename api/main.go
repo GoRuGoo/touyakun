5fc4fbd0c8691e278d9e04a8ee1f799d55b31eaa
GoRuGoo <dachanyr8989@gmail.com>
2024-05-02 13:29:18 +0900

    wip: GoによるCRONの実行処理実装中

--- api/main.go
+++ api/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"github.com/robfig/cron/v3"
 	"log"
 	"net/http"
+	"os"
+	"touyakun/controllers"
 	"touyakun/router"
 )
 
 func main() {
+	//リクエスト単体で実行するのにルーターは必要ないのでcontrollerがrouter代わり
+	nc := controllers.InitializeNotificationController(os.Getenv("CHANNEL_TOKEN"))
+
+	c := cron.New()
+	c.AddFunc("@every 5s", nc.NotificationController)
+	c.Start()
+
 	router.InitializeRouter()
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
